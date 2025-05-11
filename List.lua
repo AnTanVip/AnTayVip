e4282c3dd2c5685e953bc6ddb4ae36c14e50ecad
AnTayVip <antayvip@gmail.com>
2025-05-11 15:16:19 +0700

    Update List.lua

--- List.lua
+++ List.lua
@@ -1,4 +1,6 @@
-local UniverseID = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://apis.roblox.com/universes/v1/places/"..game.PlaceId.."/universe")).universeId
-if game.PlaceId == 116495829188952 or game.PlaceId == 70876832253163 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/AnTanVip/AnTayVip/refs/heads/main/Dead-Raids.lua"))()
-end
+local Games = {
+    [116495829188952] = "https://raw.githubusercontent.com/AnTanVip/AnTayVip/refs/heads/main/Dead-Rails.lua",
+    [70876832253163] = "https://raw.githubusercontent.com/AnTanVip/AnTayVip/refs/heads/main/Dead-Rails.lua",
+}
+
+return Games
